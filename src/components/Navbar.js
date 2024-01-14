a18913cde9fefb459b4899fcb68a1e8b0e00eb94
thiru1308 <tkumaran59@gmail.com>
2024-01-14 12:13:43 +0530

    1st commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import "./NavbarStyles.css";
+import { Menuitems } from "./Menuitems";
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+  return (
+    <nav className="NavbarItems">
+      <h1 className="navbar-logo">Trippy</h1>
+      <div className="Menu-icons">
+        <i
+          className={clicked ? "fas fa-bars" : "fas fa-times"}
+          onClick={handleClick}
+        ></i>
+      </div>
+      <ul className={clicked ? "nav-menu" : "nav-menu active"}>
+        {Menuitems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                <i className={item.icon}></i>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+        <button>Sign Up</button>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
